bcb13eb34662e692d2c048050da3a38fd13c01f3
Neil Harvey <neil.harvey61@gmail.com>
2020-09-13 13:36:17 +0100

    code edit adding user.

--- archinstall.sh
+++ archinstall.sh
@@ -340,7 +340,7 @@ setusrpriv () {
     arch-chroot /mnt useradd -m -g users -G wheel,audio,video,storage,power,input,optical,sys,log,network,floppy,scanner,rfkill,lp,adm -s /bin/bash $USERVAR
     echo
     arch-chroot /mnt echo ""${USERVAR}":"${PASSVAR}"" | chpasswd --root /mnt
-    arch-chroot /mnt echo "root:"${PASSVAR1}"" | chpasswd --root /mnt
+    arch-chroot /mnt echo "root:"${PASSVAR}"" | chpasswd --root /mnt
     clear
     echo
     echo "setting up Sudo Priverlages..."

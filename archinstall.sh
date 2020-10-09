c6272fb8ad75a7a305eecb45d495bfb267dc8688
Neil Harvey <neil.harvey61@gmail.com>
2020-10-09 19:56:57 +0100

    error handler

--- archinstall.sh
+++ archinstall.sh
@@ -35,14 +35,6 @@ welcomeb () {
 }
 #
 #
-handlerr () {
-clear
-set -uo pipefail
-trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
-clear
-}
-#
-#
 keyboard () {
     clear
     echo
